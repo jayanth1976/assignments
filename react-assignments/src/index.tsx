1085413a2a83003aa2606c47191b1e6143d1239e
jayanth1976 <118813078+jayanth1976@users.noreply.github.com>
2023-03-01 17:39:03 +0530

    complete react-parent-child assignment (#5)

--- react-assignments/src/index.tsx
+++ react-assignments/src/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./Theme";
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
